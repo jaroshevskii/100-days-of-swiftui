07abe88e7e77dee9f08c61321237fa9763db57a1
jaroshevskii <jaroshevskiisasha@gmail.com>
2023-01-09 18:04:14 +0200

    Add Checkpoint1

---
+++ Sources/Checkpoint1/main.swift
@@ -0,0 +1,15 @@
+//
+// 100 Days of SwiftUI
+// Checkpoint 1
+//
+
+// Set the temperature in Celsius
+let temperatureInCelsius = 25.0
+// Convert temperature from Celsius to Fahrenheit
+let temperatureInFahrenheit = temperatureInCelsius * 9 / 5 + 32
+
+// Prints both temperatures
+print("""
+Temperature in Celsius:    \(temperatureInCelsius)°C
+Temperature in Fahrenheit: \(temperatureInFahrenheit)°F
+""")
