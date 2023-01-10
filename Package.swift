ec12bb5b72f2f004f526949d54770da226b99b03
jaroshevskii <jaroshevskiisasha@gmail.com>
2023-01-10 19:58:29 +0200

    Add checkpoint 2

--- Package.swift
+++ Package.swift
@@ -12,6 +12,7 @@ let package = Package(
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages this package depends on.
-    .executableTarget(name: "Checkpoint1")
+    .executableTarget(name: "Checkpoint1"),
+    .executableTarget(name: "Checkpoint2")
   ]
 )
---
+++ Sources/Checkpoint2/main.swift
@@ -0,0 +1,14 @@
+//
+// 100 Days of SwiftUI
+// Checkpoint 2
+//
+
+// Names of all Jojo's.
+let jojoNames = ["Jonathan", "Joseph", "Jotaro", "Josuke", "Giorno", "Jolyne", "Johnny", "Josuke"]
+print(jojoNames, terminator: "\n\n")
+
+let nameCount = jojoNames.count
+print("Number of Jojo names: \(nameCount)")
+
+let uniqueNameCount = Set(jojoNames).count
+print("Number of unique Jojo names: \(uniqueNameCount)")
